3543c70712c850022676e474277ad99834a5a2b2
김정우 <jwo0o0358@gmail.com>
2025-10-28 00:10:57 +0900

    first commit

---
+++ src/main/java/com/ucamp/project/UcampProjectServerApplication.java
@@ -0,0 +1,13 @@
+package com.ucamp.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcampProjectServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UcampProjectServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ucamp/project/UcampProjectServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucamp.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcampProjectServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
