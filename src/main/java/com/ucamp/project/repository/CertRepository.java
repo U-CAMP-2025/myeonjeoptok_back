5adcb395d8f38b42c21338e9d7b4fc0952b4a378
HongSaek-Ko <bh1152@naver.com>
2025-10-29 18:51:37 +0900

    관리자 -> 유저 목록 조회(유저+시뮬+인증), 유저 '인증' 목록, 유저 인증상태 변경

---
+++ src/main/java/com/ucamp/project/repository/CertRepository.java
@@ -0,0 +1,14 @@
+package com.ucamp.project.repository;
+
+import com.ucamp.project.model.Certificate;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CertRepository extends JpaRepository<Certificate, Long> {
+    Optional<Certificate> findByUserUserId(@Param("userId") Long userId);
+
+}
