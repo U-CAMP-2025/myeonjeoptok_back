9dac0d3e68df29ae935ae51e5c5ba53a058158fc
sinhw0006 <sinhw0006@naver.com>
2025-10-31 11:06:14 +0900

    인터뷰터 이미지 등록 이미지 매핑

---
+++ src/main/java/com/ucamp/project/service/FileService.java
@@ -0,0 +1,47 @@
+package com.ucamp.project.service;
+
+import com.nimbusds.jose.util.Pair;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+
+@Service
+@Slf4j
+public class FileService {
+
+    public Pair<String,Resource> getImg(String type, String id) {
+
+
+        String url;
+        Resource imageResource;
+        String contentType;
+
+        if("cert".equals(type)){
+            url = "src/main/resources/static/image/"+type+"/" + id;
+        } else {
+            url = "src/main/resources/static/image/"+type+"/" + id + ".png";
+        }
+
+        try {
+            Path path = Paths.get(url);
+            imageResource = new UrlResource(Objects.requireNonNull(path).toUri());
+            contentType = Files.probeContentType(path);
+        } catch (IOException e) {
+            throw new RuntimeException("이미지 처리 실패");
+        }
+
+
+        return Pair.of(contentType, imageResource);
+
+    }
+}
