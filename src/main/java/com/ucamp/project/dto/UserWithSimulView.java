eb1a3e6b2e4146eb632d1363f08510ea241eb4d3
HongSaek-Ko <bh1152@naver.com>
2025-11-04 23:02:51 +0900

    회원 조회 로직 변경, 관리자 합격자 승인 로직 변경 (#105)

---
+++ src/main/java/com/ucamp/project/dto/UserWithSimulView.java
@@ -0,0 +1,9 @@
+package com.ucamp.project.dto;
+
+public interface UserWithSimulView {
+    String getTitle();
+    String getNickname();
+    String getEmail();
+    String getCompletedAt();
+    String getStatus();
+}
--- src/main/java/com/ucamp/project/service/AdminService.java
+++ src/main/java/com/ucamp/project/service/AdminService.java
@@ -3,6 +3,8 @@ package com.ucamp.project.service;
 import com.ucamp.project.dto.UserWithSimulDTO;
 import com.ucamp.project.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,7 +15,17 @@ public class AdminService {
     private final UserRepository userRepository;
     
     // 시뮬레이션 처리 상태 목록으로 조회
-    public List<UserWithSimulDTO> findAllTranscriptionStatus() {
-        return userRepository.findAllTranscriptionStatus();
+//    public List<UserWithSimulDTO> findAllTranscriptionStatus() {
+//        return userRepository.findAllTranscriptionStatus();
+//    }
+    public Page<UserWithSimulDTO> findAllTranscriptionStatus(Pageable pageable) {
+        return userRepository.findAllTranscriptionStatus(pageable)
+                .map(v -> new UserWithSimulDTO(
+                        v.getTitle(),
+                        v.getNickname(),
+                        v.getEmail(),
+                        v.getCompletedAt(),
+                        v.getStatus()
+                ));
     }
 }
