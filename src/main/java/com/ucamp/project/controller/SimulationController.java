a4a7c9885f9386889b16522bf318b18796b4cd05
sinhw0006 <sinhw0006@naver.com>
2025-11-04 22:17:13 +0900

    시뮬 수정중

--- src/main/java/com/ucamp/project/controller/SimulationController.java
+++ src/main/java/com/ucamp/project/controller/SimulationController.java
@@ -164,4 +164,17 @@ public class SimulationController {
                 .build();
     }
 
+    @PatchMapping("/{simulationId}/{qaCount}")
+    public ApiResponse<?> stopSimulation(@PathVariable Long simulationId, @PathVariable Long qaCount){
+
+        simulationService.end(simulationId,qaCount);
+
+        return ApiResponse.builder()
+                .code(200)
+                .message("success")
+                .data("ok")
+                .build();
+
+    }
+
 }
--- src/main/java/com/ucamp/project/repository/SimulationRepository.java
+++ src/main/java/com/ucamp/project/repository/SimulationRepository.java
@@ -4,6 +4,8 @@ import com.ucamp.project.model.Post;
 import com.ucamp.project.model.Simulation;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,5 +17,30 @@ public interface SimulationRepository extends JpaRepository<Simulation, Long> {
     Optional<Simulation> findBySimulationId(Long simulationId);
 
     List<Simulation> findByUser_UserIdOrderBySimulationIdDesc(Long userId);
+
+
+    @Query(value = """
+            SELECT *
+            FROM (
+                SELECT s.*,
+                       ROW_NUMBER() OVER (PARTITION BY s.post_id ORDER BY s.simulation_created_at DESC) AS rn
+                FROM SIMULATION s
+                WHERE s.user_id = :userId
+                  AND s.simulation_status = 'SUCCESS'
+            ) t
+            WHERE t.rn = 1
+            """, nativeQuery = true)
+    List<Simulation> findLatestSimulationPerPost(@Param("userId") Long userId);
+
+    @Query(value = """
+        SELECT COUNT(*)
+        FROM SIMULATION s
+        WHERE s.user_id = :userId
+          AND s.simulation_status = 'SUCCESS'
+          AND s.post_id = :postId
+        """, nativeQuery = true)
+    Long countSuccessByUserAndPost(@Param("userId") Long userId,
+                                   @Param("postId") Long postId);
+
 
 }
