03bbaa2c3f7cbc08ea28c4a1d58bc86819c36dce
sinhw0006 <sinhw0006@naver.com>
2025-11-05 09:30:56 +0900

    시뮬 완료시간 전송

--- src/main/java/com/ucamp/project/dto/SimulationRecordItemDto.java
+++ src/main/java/com/ucamp/project/dto/SimulationRecordItemDto.java
@@ -3,6 +3,7 @@ package com.ucamp.project.dto;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
@@ -12,6 +13,7 @@ public class SimulationRecordItemDto {
     private String simulationStatus; // INPROGRESS / COMPLETED
     private PostBrief post;
     private Long count;
+    private LocalDateTime completedAt;
 
     @Getter
     @Builder
