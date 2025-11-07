a9c083f4f348e8ea69c4c40f41636ad2d74f7c0e
김정우 <jwo0o0358@gmail.com>
2025-11-07 14:27:05 +0900

    fix: cors allowed origins 추가

--- src/main/java/com/ucamp/project/config/SecurityConfig.java
+++ src/main/java/com/ucamp/project/config/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://myeonjeoptok.vercel.app"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://myeonjeoptok.vercel.app", "https://mjtok.shop", "https://www.mjtok.shop"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("Set-Cookie"));
