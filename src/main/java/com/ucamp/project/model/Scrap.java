53573175bf8151db1b832008a198d82cc44f0fa6
sinhw0006 <sinhw0006@naver.com>
2025-11-07 16:27:50 +0900

    Scrap 온딜리트 설정

--- src/main/java/com/ucamp/project/model/Scrap.java
+++ src/main/java/com/ucamp/project/model/Scrap.java
@@ -2,6 +2,9 @@ package com.ucamp.project.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
 import java.io.Serializable;
 
 @Entity
@@ -17,10 +20,12 @@ public class Scrap implements Serializable {
     @Id
     @ManyToOne
     @JoinColumn(name = "post_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private Post post;
 
     @Id
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private User user;
 }
